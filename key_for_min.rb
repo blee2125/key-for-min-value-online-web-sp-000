74da3bc3cb73b4c577ce544ad21c008bff6ebfc7
favorable-device-5158 <blee2125@gmail.com>
2019-08-22 18:06:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.collect do |object, number|
+  name_hash.each do |object, number|
     if number[1] < number[2] || number[3]
       return object[1]
     elsif number[2] < number[3]

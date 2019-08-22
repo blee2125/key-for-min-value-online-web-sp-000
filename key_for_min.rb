87b8a732074bb13cd4ca0e70eae9f05b47507876
favorable-device-5158 <blee2125@gmail.com>
2019-08-22 18:06:56 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,12 +3,12 @@
 
 def key_for_min_value(name_hash)
   name_hash.each do |object, number|
-    if number[1] < number[2] || number[3]
+    if number[0] < number[1] || number[2]
+      return object[0]
+    elsif number[1] < number[2]
       return object[1]
-    elsif number[2] < number[3]
-      return object[2]
     else
-      return object[3]
+      return object[2]
     end
   end
 end
